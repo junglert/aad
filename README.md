111d91715e1074f33014ffef9c95bb64a376fea9
Johannes Unglert <unglert@b1-systems.de>
2020-03-23 10:30:04 +0100

    First inital commit: Add script and Readme

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# AAD
-Ansible automated deployment
+# automated ansible deployment / aad
+Automated deployment with Ansible
+### For now, you need to manually install ansible
+### Please install it via your packagemanager (yum, zypper, apt, dpkg, etc...)
+***
+# Configuring ansible
+  
+  ## Edit the ansible hosts file in /etc/ansible/
+  
+  Delete the # in front of [webservers]
+  
+          ##[webservers]
+          ##120.0.0.0
+          ##120.0.0.1
+          
+  and add the IP of your remote host beneath the default ips
+  
+  ## Generate a new ansible-ssh-key
+  
+      ssh-keygen
+      
+  follow the instructions and create your new key
+  
+  ## Copy your new key onto the remote host/s
+  
+      ssh-copy-id -i <NAME OF YOUR KEY> hostname@ip
+      
+  Everything should be configured now and we can start with the main installation process
+***
+# How to install and get started
+  
+  Install this git-repo:
+  
+         git clone https://github.com/junglert/aad.git
+
+    
+  Exec the script, you may have to change the permissions with 
+              
+          ./automated_deployment.sh or bash automated_deployment.sh        
+    
+  The script automatically creates and copies needed folders and files into your home directory.
+  If everything already is in place, the script will skip this part and proceed with the next step.
+  
+  In the next step you will be asked how the remote host is named, type in the username and proceed with enter.
+  
+  The script now asks if you want to give any additional parameters:
+  
+           
+  
+  You will be asked to enter the password of the remote host, enter the password and ansible should now setup the website.
+  
+# What else can you do with this script?
+
+  You can use any .yml playbook and deploy it via this script.
+  Just simply change the path of the variable.
+  
+***    
+# Future plans
+   
+   - automatically installes ansible if not already installed
+     
+   - creating a wiki / manual 
+    
---
+++ automated_deployment.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#author: junglert
+#mail:
+#date: 19.02.2020
+#license: GPL-3.0
+#summary: bash script which deploys a website via ansible
+
+
+#variables
+user="$USER"
+site_name="index.html"
+keyfile="/home/$user/.ssh/ansible_key"
+ansible_path="/home/$user/ansible"
+playbook="$ansible_path/apache.yml"
+index="$ansible_path/$site_name"
+splitter="----------------------------------------------------------------"
+
+
+#generate folder for files and addons, if not already generated
+echo "
+Initial check if folder ansible exists
+"
+if [ -d "$ansible_path" ];
+    then
+            echo -e "Folder already exists \e[32m\u2714 \e[39m
+            "
+            echo $splitter
+    else
+            echo "Creating ansible folder in /home/$user/
+            "
+            mkdir /home/$user/ansible
+            echo $splitter
+fi
+
+
+#check if playbook exists in the ansible folder
+echo "
+Initial check if playbook exists in the ansible folder
+"
+if test -f $playbook;
+    then
+            echo -e "$playbook exists \e[32m\u2714 \e[39m
+            "
+            echo $splitter
+    else
+            echo "Copying playbook into ansible folder
+            "
+            cp playbooks/apache.yml $ansible_path/
+            echo $splitter
+fi
+
+
+#check if index.html exists in the ansible folder
+echo "
+Initial check if index.html exists in the ansible folder
+"
+if test -f $index;
+    then
+            echo -e "$index exists \e[32m\u2714 \e[39m
+            "
+            echo $splitter
+    else
+            echo "Copying index.html into ansible folder
+            "
+            cp $site_name $ansible_path
+            echo $splitter
+fi
+
+
+#collect data for ansible
+read -p "Username of remote host: " remote_host
+echo "
+Add your own additional parameters - leave blank for normal use"
+read -p "additional parameters: " par_var
+
+
+#ansible playbook execution
+ansible-playbook -u $remote_host -i /etc/ansible/hosts -K $par_var --key-file=$keyfile $playbook
